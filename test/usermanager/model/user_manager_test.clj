a6dce3edcfcefe017c660c2fc2c89e5026a24fe9
Sean Corfield <sean@corfield.org>
2020-05-30 23:06:24 -0700

    Clean up the experiment

--- test/usermanager/model/user_manager_test.clj
+++ test/usermanager/model/user_manager_test.clj
@@ -1,4 +1,4 @@
-;; copyright (c) 2019 Sean Corfield, all rights reserved
+;; copyright (c) 2019-2020 Sean Corfield, all rights reserved
 
 (ns usermanager.model.user-manager-test
   "These tests use H2 in-memory."
@@ -12,59 +12,6 @@
 (def ^:private db-spec {:dbtype "h2:mem"
                         :dbname "usermanager_test"
                         :database_to_upper false})
-
-(comment
-  (require '[clojure.datafy :as d])
-  (require '[clojure.core.protocols :as p])
-  (require '[next.jdbc.result-set :as rs])
-  (def con (.getConnection (jdbc/get-datasource db-spec)))
-  (.close con)
-  (.getMetaData (.getCatalogs (.getMetaData con)))
-  ;; this is not possible:
-  (.getMetaData (jdbc/execute! con ["select * from addressbook"]))
-
-  (def column-meta
-    {:columnLabel (fn [^java.sql.ResultSetMetaData o ^Integer i] (.getColumnLabel o i))})
-
-  (extend-protocol p/Datafiable
-    java.sql.Connection
-    (datafy [this]
-      (with-meta (merge {} (bean this))
-        {`p/nav (fn [coll k v]
-                  (if (= :metaData k)
-                    (.getMetaData this)
-                    v))}))
-    java.sql.DatabaseMetaData
-    (datafy [this]
-      (with-meta (merge {} (bean this))
-        {`p/nav (fn [coll k v]
-                  (condp = k
-                    :catalogs
-                    (rs/datafiable-result-set (.getCatalogs this)
-                                              (.getConnection this)
-                                              {})
-                    :clientInfoProperties
-                    (rs/datafiable-result-set (.getClientInfoProperties this)
-                                              (.getConnection this)
-                                              {})
-                    :schemas
-                    (rs/datafiable-result-set (.getSchemas this)
-                                              (.getConnection this)
-                                              {})
-                    :tableTypes
-                    (rs/datafiable-result-set (.getTableTypes this)
-                                              (.getConnection this)
-                                              {})
-                    :typeInfo
-                    (rs/datafiable-result-set (.getTypeInfo this)
-                                              (.getConnection this)
-                                              {})
-                    v))}))
-    java.sql.ResultSetMetaData
-    (datafy [this]
-      {:columnCount (.getColumnCount this)
-       :columns (mapv (fn [i] (reduce-kv (fn [m k f] (assoc m k (f this i))) {} column-meta)) (range 1 (inc (.getColumnCount this))))}))
-  nil)
 
 (defn- with-test-db
   "A test fixture that sets up an in-memory H2 database for running tests."
