87f100992132e0f793a9f186a33a137fcac102e2
Sean Corfield <sean@worldsingles.com>
2021-03-31 18:26:03 -0700

    Add three missing core test stubs

---
+++ components/database/test/usermanager/database/core_test.clj
@@ -0,0 +1,6 @@
+(ns usermanager.database.core-test
+  (:require [clojure.test :as test :refer [deftest is testing]]
+            [usermanager.database.core :as sut]))
+
+(deftest dummy-test
+  (is (= 1 1)))
---
+++ components/web-server/test/usermanager/web_server/core_test.clj
@@ -0,0 +1,6 @@
+(ns usermanager.web-server.core-test
+  (:require [clojure.test :as test :refer [deftest is testing]]
+            [usermanager.web-server.core :as sut]))
+
+(deftest dummy-test
+  (is (= 1 1)))
