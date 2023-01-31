1a58880c692a68a7f46796fee66d13dabe170300
Sean Corfield <sean@corfield.org>
2023-01-30 19:09:37 -0800

    update copyright and clj-kondo config

--- src/usermanager/model/user_manager.clj
+++ src/usermanager/model/user_manager.clj
@@ -1,4 +1,4 @@
-;; copyright (c) 2019-2022 Sean Corfield, all rights reserved
+;; copyright (c) 2019-2023 Sean Corfield, all rights reserved
 
 (ns usermanager.model.user-manager
   "The model for the application. This is where the persistence happens,
